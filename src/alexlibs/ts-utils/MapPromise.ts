859572709262a7795554c1c88e71d714c4390ee7
eletallbetagouv <107104509+eletallbetagouv@users.noreply.github.com>
2022-06-29 19:49:40 +0200

    flatten ts-utils files

--- src/alexlibs/ts-utils/MapPromise.ts
+++ src/alexlibs/ts-utils/MapPromise.ts
@@ -1,4 +1,4 @@
-import {PromiseFnResult} from '../Common'
+import {PromiseFnResult} from './Common'
 
 /**
  * Map a function returning a Promise without having to redefined his parameters
--- src/alexlibs/ts-utils/common/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './Common'
-export * from './enum/Enum'
-export * from './env/Env'
-export * from './env/EnvParser'
-export * from './fnSwitch/FnSwitch'
-export * from './map/Map'
-export * from './mapPromise/MapPromise'
