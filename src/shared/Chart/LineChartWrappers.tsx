89853d8830ce75be4169680e1d75e4119a845864
eletallbetagouv <107104509+eletallbetagouv@users.noreply.github.com>
2022-07-14 17:52:23 +0200

    made panel like Ingrid wanted

--- src/shared/Chart/LineChartWrappers.tsx
+++ src/shared/Chart/LineChartWrappers.tsx
@@ -7,7 +7,7 @@ import {useEffect, useState} from 'react'
 export const toPercentage = (numerator: CountByDate[], denominator: CountByDate[]): CountByDate[] => {
   return numerator.map<CountByDate>((k, i) => ({
     date: k.date,
-    count: denominator[i] && denominator[i].count > 0 ? Math.round((k.count / denominator[i].count) * 100) : 0,
+    count: denominator[i] && denominator[i].count > 0 ? Math.min(Math.round((k.count / denominator[i].count) * 100), 100) : 0,
   }))
 }
 
